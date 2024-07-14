8ee9a7698fac89a51e763a3d7758442d5bfd00ba
DanielQLL <133980732+DanielQLL@users.noreply.github.com>
2024-07-14 14:19:13 -0500

    UwU

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('Nuevo cliente conectado');
+
+  socket.on('move', (data) => {
+    io.emit('move', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Cliente desconectado');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
